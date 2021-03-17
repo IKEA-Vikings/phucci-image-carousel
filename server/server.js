f2693af27ece87d0fae93f93081fa140d4d048cd
phucciTr <miabam26@gmail.com>
2021-03-17 13:55:52 -0700

    Edit server to use absolute path for static contents

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const db = require('./database/index').db;
 const seeder = require('./seeding-script/seeder');
+const path = require('path');
 
 
 const app = express();
 const port = 3004;
-
-app.use(express.static(__dirname + '/../public/'));
+app.use(express.static(path.join(__dirname, '/../public')));
 seeder.seedIfEmpty();
 
 app.get('/images/org/:id', (req, res) => {
