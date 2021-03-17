65cb75423a51bc09a50700e823e4caafdb7fe494
phucciTr <miabam26@gmail.com>
2021-03-17 13:41:20 -0700

    Minor refactor

--- client/components/service-type/image/Image.jsx
+++ client/components/service-type/image/Image.jsx
@@ -3,10 +3,8 @@ import TableImage from './image-type/TableImage.jsx';
 import SlideImage from './image-type/SlideImage.jsx';
 
 
-const Image = ({url, slide, carousel, id, handleClick}) => {
-  return slide ?
-    <SlideImage url={url} id={id} carousel={carousel} /> :
-    <TableImage url={url} id={id} handleClick={handleClick} />;
-};
+const Image = ({url, slide, carousel, id, handleClick}) => slide ?
+  <SlideImage url={url} id={id} carousel={carousel} /> :
+  <TableImage url={url} id={id} handleClick={handleClick} />;
 
 export default Image;
