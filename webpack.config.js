d570677841dad8a7a923174deaca0149ccafbcdb
phucciTr <miabam26@gmail.com>
2021-03-17 12:10:15 -0700

    Set up client dependencies

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'client/app.jsx'),
+
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, '/public')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          },
+        }
+      },
+
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      }
+    ]
+  },
+};
