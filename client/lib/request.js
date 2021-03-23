e7712dd1e36743fea92e1ed4672874e1639543c6
phucciTr <miabam26@gmail.com>
2021-03-22 18:35:09 -0700

    Minor change to default id to 1 when there's no id param

--- client/lib/request.js
+++ client/lib/request.js
@@ -3,7 +3,7 @@ import $ from 'jquery';
 const request = {
   getOrgImages: (successCb) => {
     let productId = window.location.href.split('/')[3];
-    productId = !productId ? 0 : productId;
+    productId = !productId ? 1 : productId;
 
     $.ajax({
       url: `http://127.0.0.1:3004/images/org/${productId}`,
