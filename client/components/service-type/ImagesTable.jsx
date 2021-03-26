7d3aea1dd2fdfc1d14834848559085272e83ca15
phucciTr <miabam26@gmail.com>
2021-03-26 12:56:24 -0700

    Edit images table to only render 4 images

--- client/components/service-type/ImagesTable.jsx
+++ client/components/service-type/ImagesTable.jsx
@@ -6,7 +6,7 @@ import ImagesCarousel from './ImagesCarousel.jsx';
 const ImagesTable = ({images, handleClick}) => (
   <div className="images-table">
     <ImagesRow row={0} images={images.slice(0, 2)} handleClick={handleClick} />
-    <ImagesRow row={1} images={images.slice(2)} handleClick={handleClick} />
+    <ImagesRow row={1} images={images.slice(2, 4)} handleClick={handleClick} />
   </div>
 );
 
